6f80c758c895bd02d0356d42206c60aa9522bce8
Alex <alexzhang1931@gmail.com>
2018-08-20 16:40:03 +1000

    change credential to provided

--- internals/firebase-config.js
+++ internals/firebase-config.js
@@ -4,9 +4,13 @@ import admin from 'firebase-admin';
 let firestore = null;
 export function initializeFirebase() {
   return new Promise((resolve) => {
-
+    let privatekey = JSON.parse(JSON.stringify(process.env.KEY).split('\\\\').join('\\'));
     const firebaseApp = admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
+      credential: admin.credential.cert({
+        projectId: process.env.PROJECT_ID,
+        clientEmail: 'firebase-adminsdk-aymeb@gdgcloud-devfest.iam.gserviceaccount.com',
+        privateKey: privatekey
+      }),
       databaseURL: 'https://gdgcloud-devfest.firebaseio.com'
     });
     firestore = admin.firestore();
